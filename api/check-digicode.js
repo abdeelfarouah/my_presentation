67e1d95ed39fea34740c195f498268b035333db4
abdeelfarouah <abderrahmane.elfarouah@laposte.net>
2025-09-30 17:29:28 +0200

    variable change security

--- api/check-digicode.js
+++ api/check-digicode.js
@@ -1,13 +1,14 @@
 export default function handler(req, res) {
-  if (req.method === 'POST') {
-    const { code } = req.body;
-    if (code === '1234') { // digicode exemple
-      res.status(200).json({ success: true });
-    } else {
-      res.status(200).json({ success: false });
-    }
+  if (req.method !== "POST") {
+    return res.status(405).json({ error: "Méthode non autorisée" });
+  }
+
+  const { code } = req.body;
+
+  // Comparaison avec la variable d'environnement
+  if (code === process.env.DIGICODE) {
+    return res.status(200).json({ success: true });
   } else {
-    res.setHeader('Allow', ['POST']);
-    res.status(405).end(`Méthode ${req.method} non autorisée`);
+    return res.status(200).json({ success: false });
   }
 }
