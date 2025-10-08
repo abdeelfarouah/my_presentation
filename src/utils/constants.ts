7ee251ce3efac4d5874cc5d51fb4f51587a3f996
Abderrahmane El farouah <103990456+abdeelfarouah@users.noreply.github.com>
2025-10-08 17:16:44 +0200

    Update constants.ts

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -2,7 +2,7 @@ export const SOCIAL_LINKS = {
   GITHUB: 'https://github.com/abdeelfarouah/',
   LINKEDIN: 'https://fr.linkedin.com/in/abderrahmaneelfarouah',
   EMAIL: 'A.elfarouahDEV@outlook.fr',
-  CV_URL: 'https://raw.githubusercontent.com/abdeelfarouah/my_presentation/eb7c1bf498c8f9b173abd994e93191f6360f326f/src/abderrahmane%20EL%20FAROUAH%20CV.pdf',
+  CV_URL: 'https://raw.githubusercontent.com/abdeelfarouah/my_presentation/655a6d0e909b552be1e814e1cc23f90691a30436/src/abderrahmane-elfarouah_cv.pdf',
 };
 export const NAV_ITEMS = [
   { href: '#home', label: 'Accueil' },
