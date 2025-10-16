c9d0ec07085d6963c7b4b27a2f3532f3985ef37e
abdeelfarouah <abderrahmane.elfarouah@laposte.net>
2025-10-16 15:32:40 +0200

    fix size

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,60 @@
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-
+import type { Plugin } from 'vite';
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import viteImagemin from 'vite-plugin-imagemin';
+import { visualizer } from 'rollup-plugin-visualizer';
+   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteImagemin({
+      gifsicle: { optimizationLevel: 7 },
+      optipng: { optimizationLevel: 7 },
+      mozjpeg: { quality: 75 },
+      pngquant: { quality: [0.6, 0.8] },
+      svgo: {
+        plugins: [
+          { name: 'removeViewBox', active: false },
+          { name: 'removeEmptyAttrs', active: false },
+        ],
+      },
+    }) as unknown as Plugin,
+    visualizer({
+      open: true,
+      filename: 'dist/stats.html',
+      template: 'treemap',
+    }),
+  ],
+
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
+
   server: {
     proxy: {
       '/api': {
         target: 'http://localhost:4000',
         changeOrigin: true,
         secure: false,
+      },
+    },
+  },
+
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    minify: 'esbuild',
+    cssCodeSplit: true,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ['react', 'react-dom'],
+        },
       },
     },
   },
