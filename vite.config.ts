ba2399d7cd4e49fcd8b9da72cce09b684e7366d4
abdeelfarouah <abderrahmane.elfarouah@laposte.net>
2025-12-17 15:55:51 +0100

    fix: simplify asset filenames for production

--- vite.config.ts
+++ vite.config.ts
@@ -35,11 +35,12 @@ export default defineConfig({
     outDir: 'dist',
     assetsDir: 'assets',
     emptyOutDir: true,
+    // Disable hashing for production files
     rollupOptions: {
       output: {
-        assetFileNames: 'assets/[name]-[hash][extname]',
-        chunkFileNames: 'assets/[name]-[hash].js',
-        entryFileNames: 'assets/[name]-[hash].js',
+        assetFileNames: 'assets/[name][extname]',
+        chunkFileNames: 'assets/[name].js',
+        entryFileNames: 'assets/[name].js',
       }
     }
   },
