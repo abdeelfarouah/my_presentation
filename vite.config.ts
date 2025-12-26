e3f8d0bd4759cb6bca5418b593d43482692a5282
abdeelfarouah <abderrahmane.elfarouah@laposte.net>
2025-12-26 20:13:53 +0100

    Fix Vercel configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,29 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { visualizer } from 'rollup-plugin-visualizer';
 
 export default defineConfig({
-  base: '/dist/',
-  root: '.',
-  publicDir: 'public',
+  plugins: [react()],
+  base: '/', // Assurez-vous que c'est bien défini
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
     emptyOutDir: true,
-    rollupOptions: {
-      output: {
-        assetFileNames: 'assets/[name][extname]',
-        chunkFileNames: 'assets/[name].js',
-        entryFileNames: 'assets/[name].js'
-      }
-    }
   },
-  plugins: [
-    react(),
-    visualizer({
-      open: true,
-      gzipSize: true,
-      brotliSize: true
-    })
-  ]
 });
